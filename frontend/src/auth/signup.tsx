fde83b759f0ec853ac7ac286465a4c936ee0a037
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-11-18 10:40:17 +0530

    signup pase setup

--- frontend/src/auth/signup.tsx
+++ frontend/src/auth/signup.tsx
@@ -3,6 +3,8 @@ import React, { useState } from "react";
 import { auth } from "../context/firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import GenderCheckbox from "../components/genderCheckbox";
 
 const SignUp: React.FC = () => {
   const [email, setEmail] = useState("");
@@ -21,21 +23,70 @@ const SignUp: React.FC = () => {
 
   return (
     <form onSubmit={handleSignUp}>
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Sign Up</button>
+      <div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
+			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+				<h1 className='text-3xl font-semibold text-center text-gray-300'>
+					Sign Up <span className='text-blue-500'> ChatApp</span>
+				</h1>
+
+				<form>
+					<div>
+						<label className='label p-2'>
+							<span className='text-base label-text'>Full Name</span>
+						</label>
+						<input type='text' placeholder='John Doe' className='w-full input input-bordered  h-10' />
+					</div>
+
+					<div>
+						<label className='label p-2 '>
+							<span className='text-base label-text'>Username</span>
+						</label>
+						<input type='text' placeholder='johndoe' className='w-full input input-bordered h-10' />
+					</div>
+          <div>
+						<label className='label p-2 '>
+							<span className='text-base label-text'>email</span>
+						</label>
+						<input onChange={(e)=>{setEmail(e.target.value)}}type='text' placeholder='Email' className='w-full input input-bordered h-10' />
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Password</span>
+						</label>
+						<input
+							type='password'
+							placeholder='Enter Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+
+					<div>
+						<label className='label'>
+							<span className='text-base label-text'>Confirm Password</span>
+						</label>
+						<input onChange={(e)=>{setPassword(e.target.value)}}
+							type='password'
+							placeholder='Confirm Password'
+							className='w-full input input-bordered h-10'
+						/>
+					</div>
+
+					<GenderCheckbox />
+
+					<Link
+						to={"/login"}
+						className='text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-white'
+					>
+						Already have an account?
+					</Link>
+
+					<div>
+						<button className='btn btn-block btn-sm mt-2 border border-slate-700'>Sign Up</button>
+					</div>
+				</form>
+			</div>
+		</div>
     </form>
   );
 };
