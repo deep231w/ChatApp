9978bff2ee8c034a7483537f772f01f1c6b01d07
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-11-27 15:38:19 +0530

    signup/signin routes updated

--- frontend/src/context/authContext.tsx
+++ frontend/src/context/authContext.tsx
@@ -1,38 +1,3 @@
-// // src/context/AuthContext.tsx
-// import React, { createContext, useContext, useEffect, useState } from "react";
-// import { auth } from "./firebase";
-// import { onAuthStateChanged, User } from "firebase/auth";
-
-// interface AuthContextType {
-//   currentUser: User | null;
-// }
-
-//  const AuthContext = createContext<AuthContextType | undefined>(undefined);
-
-// export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-//   const [currentUser, setCurrentUser] = useState<User | null>(null);
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       setCurrentUser(user);
-//     });
-//     return unsubscribe;
-//   }, []);
-
-//   return (
-//     <AuthContext.Provider value={{ currentUser }}>
-//       {children}
-//     </AuthContext.Provider>
-//   );
-// };
-
-// export const useAuth = () => {
-//   const context = useContext(AuthContext);
-//   if (!context) {
-//     throw new Error("useAuth must be used within an AuthProvider");
-//   }
-//   return context;
-// };
 
 // src/context/AuthContext.tsx
 import React, { createContext, useContext, useEffect, useState } from "react";
