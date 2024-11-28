950ca6c2b51487bd10a127e3cb2db7c36844aa91
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2024-11-28 21:47:22 +0530

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,19 +7,40 @@ import {Server} from "socket.io";
 const app = express();
 app.use(express.json());
 const server= createServer(app);
-const io=new Server(server);
+const io=new Server(server,{
+  cors:{
+    origin:'http://localhost:5173',
+    methods:['POST','GET'],
+    credentials:true
+  }
+});
 
 app.use(cors({
    origin: 'http://localhost:5173',
    methods:['POST','GET','DELETE','PUT'],
    credentials:true
 }))
+
+io.on("connection", (socket)=>{
+    console.log("user connected", socket.id);
+
+    socket.on("send_message",(message)=>{
+      io.emit(message);
+    })
+    
+    socket.on("disconnect",()=>{
+      console.log(`${socket.id} is disconnected`);
+
+    })
+
+})
+
 app.use("/api/user",useRoute);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
+server.listen(3000, () => {
   console.log("Server running on port 3000");
 });
