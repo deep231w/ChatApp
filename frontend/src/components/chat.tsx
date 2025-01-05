24d74a91415a6682f66d6de62601f81f6ada45e1
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-01-05 18:53:32 +0530

    front end ui updated

--- frontend/src/components/chat.tsx
+++ frontend/src/components/chat.tsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from "react";
 import { useSocket } from "../context/socketContext";
-
+import { useAuth } from "../context/authContext";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
 export const Chat = () => {
   const { socket } = useSocket();
+  const [reciverId, setReciverId]=useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
-
+  const {currentUser}= useAuth();
   const handleSendMessage = () => {
     if (socket && message.trim()) {
+
       console.log("Sending message:", message);
-      socket.emit("send_message", message);
+      
+      socket.emit("send_message", {
+        senderId:currentUser?.uid,
+        reciverId,
+        message
+      });
       setMessage(""); 
     }
   };
@@ -38,15 +47,16 @@ export const Chat = () => {
         ))}
       </div>
       <div>
-        <input
+        <Input
           type="text"
           placeholder="Type Message..."
           value={message}
           onChange={(e) => {
             setMessage(e.target.value);
           }}
+          className="p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
-        <button onClick={handleSendMessage}>Send</button>
+        <Button onClick={handleSendMessage}>Send</Button>
       </div>
     </>
   );
