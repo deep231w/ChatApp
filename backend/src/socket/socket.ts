c68e5cdcc18093e41f5d35763aee0155d4830619
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-01-05 18:52:50 +0530

    socket fixed

--- backend/src/socket/socket.ts
+++ backend/src/socket/socket.ts
@@ -1,5 +1,6 @@
 import { Server as SocketServer } from "socket.io";
 import { Server as HttpServer } from "http";
+import { object } from "zod";
 
 let io: SocketServer; // Exportable Socket.IO instance for future use
 
@@ -11,20 +12,47 @@ export const initializeSocket = (server: HttpServer) => {
       credentials: true,
     },
   });
+  interface OnlineUsers{
+    [userid:string]:string;
+  }
+  const onlineusers:OnlineUsers={};
 
   io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+    //register socket id
+    socket.on("register",(userId:string)=>{
+        onlineusers[userId]=socket.id;
+        console.log(`${userId}=${socket.id}`);
+    })
 
+    socket.on("private_message", ({senderId, reciverId, message})=>{
+        const reciverSocketid=onlineusers[reciverId];
+        if(reciverSocketid){
+            io.to(reciverSocketid).emit("recive_message",{
+                senderId,
+                message
+            })
+        }
+    })
+    socket.on("disconnect",()=>{
+        for(const [userId,socketId] of Object.entries(onlineusers)){
+            if(socketId==socket.id){
+                delete onlineusers[userId];
+                console.log(`${userId} disconnected`)
+                break;
+            }
+        }
+    })
     // Handle send_message event
-    socket.on("send_message", (message) => {
-      console.log("Backend message:", message);
-      io.emit("message", message); // Broadcast to all clients
-    });
+    // socket.on("send_message", (message) => {
+    //   console.log("Backend message:", message);
+    //   io.emit("message", message); // Broadcast to all clients
+    // });
 
     // Handle disconnection
-    socket.on("disconnect", () => {
-      console.log(`${socket.id} disconnected`);
-    });
+    // socket.on("disconnect", () => {
+    //   console.log(`${socket.id} disconnected`);
+    // });
   });
 
   console.log("Socket.IO initialized");
