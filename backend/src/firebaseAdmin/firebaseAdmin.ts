fadf6f010da60f6cc834c0960e12f9d0307e5568
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-02-16 00:21:38 +0530

    fixed

--- backend/src/firebaseAdmin/firebaseAdmin.ts
+++ backend/src/firebaseAdmin/firebaseAdmin.ts
@@ -1,12 +1,21 @@
 import admin from "firebase-admin";
-import dotenv from  "dotenv"
+//import serviceAccount from "./../config/serviceAccountKey.json";
+ const serviceAccount = require("../config/serviceAccountKey");
 
-dotenv.config();
+// import dotenv from "dotenv";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS || "{}");
+// dotenv.config();
+
+// const serviceAccount= process.env.FIREBASE_ADMIN_CREDENTIALS; 
+
+console.log(serviceAccount);
+
+if(!serviceAccount){
+  console.log("no service account key ");
+}
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount), 
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount), // Fix: Type assertion
   databaseURL: "https://chatapp-6a195-default-rtdb.firebaseio.com",
 });
 
