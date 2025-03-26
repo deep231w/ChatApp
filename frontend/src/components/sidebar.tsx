15e89a6915beeb799a37a6d0cd7220f52caf6190
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-03-26 15:17:03 +0530

    updated

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -11,27 +11,28 @@ export const Sidebar = ({ onSelectuser, onRecivername }: { onSelectuser: (id: st
     console.log("selected user id in sidebar= ",selectedId)
     return (
         <div className={`flex h-screen`}>
-            <div className="w-64 bg-gray-100 border-r border-gray-200 shadow-sm flex-1 overflow-y-auto">
+            <div className="w-64 bg-gray-100 border-r border-gray-200 shadow-sm flex-1">
                 <div className="p-4 border-b border-gray-300">
                     <h2 className="text-lg font-semibold text-gray-800">Available Users</h2>
                 </div>
-                <ul className="p-4 space-y-2">
-                    {users.length > 0 ? (
-                        users.map((user) => (
-                            <li
-                                key={user.id}
-                                onClick={() => setSelectedUserId(user.id)} // Now setting selected user
-                                
-                                className={`cursor-pointer ${selectedId === user.id ? "font-bold text-blue-500" : "text-gray-800"}`}
-                            >
-                                {user.firstName}
-                            </li>
+                <div>
+                    <ul className="p-4 space-y-2">
+                        {users.length > 0 ? (
+                            users.map((user) => (
+                                <li
+                                    key={user.id}
+                                    onClick={() => setSelectedUserId(user.id)} // Now setting selected user
+                                    className={`cursor-pointer ${selectedId === user.id ? "font-bold text-blue-500" : "text-gray-800"}`}
+                                >
+                                    {user.firstName}
+                                </li>
                         ))
-                    ) : (
+                        ) : (
                         <li className="text-gray-500 italic">No users Available</li>
-                    )}
-                </ul>
-                <div className="p-4 border-t border-gray-300">
+                        )}
+                    </ul>
+                </div>
+                <div className="p-4 border-t border-gray-300 ">
                     <UserMenu/>
                 </div>
             </div>
