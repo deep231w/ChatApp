f221b5f120ff1687a31911bfe7962e07145d5224
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-04-03 21:13:06 +0530

    user name adde dto userMenu

--- frontend/src/components/userMenu.tsx
+++ frontend/src/components/userMenu.tsx
@@ -1,12 +1,14 @@
 import { User } from "lucide-react";
 import { DropdownMenuO } from "./ui/DropDownMenu";
 import { DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./components/ui/dropdown-menu";
+import { useAuth } from "../context/authContext";
 export default function UserMenu() {
     console.log("UserMenu is rendering!");
+    const {localstorageUser}=useAuth();
     return (
         <div className="flex items-center justify-between">
-            <div>
-                UserName
+            <div className="text-lg font-bold text-blue-500">
+                {localstorageUser.firstName}
             </div>
             <div>
             <DropdownMenuO>
