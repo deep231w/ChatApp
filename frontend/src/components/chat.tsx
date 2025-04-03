f88a35e8cdcccf127dc13d3f91f7d93499d45d06
Deepak kumar maharana <100968930+deep231w@users.noreply.github.com>
2025-04-03 23:27:43 +0530

    unnecessary lines removed

--- frontend/src/components/chat.tsx
+++ frontend/src/components/chat.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useAuth } from "../context/authContext";
 import { Button } from "./components/ui/button";
 import { Input } from "./components/ui/input";
 import { useUsersContext } from "../context/usersContext";
