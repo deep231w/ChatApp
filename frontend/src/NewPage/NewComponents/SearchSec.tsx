8027188285a9e813e956727749c407fdfeda680f
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-06-10 00:15:51 +0530

    updated search se

--- frontend/src/NewPage/NewComponents/SearchSec.tsx
+++ frontend/src/NewPage/NewComponents/SearchSec.tsx
@@ -1,11 +1,21 @@
+import { SearchIcon } from "lucide-react";
+
 export default function SearchSec(){
     return(
         <div
-            className="flex justify-center"
+            className="flex justify-center relative"
         >
             <input
-                className="w-full m-3 h-10"
-                type="Search" />
+                className="w-full m-3 h-10 border-black rounded-[30px] bg-white text-black p-2"
+                type="Search" 
+                placeholder="Search User"
+            />
+            {/* search icon */}
+            <div 
+                className="absolute right-3 top-1/2 -translate-y-1/2 pr-4 border-black rounded-[30px] bg-gray-400 p-2 "
+            >
+                <SearchIcon/>
+            </div>
         </div>
     )
 }
