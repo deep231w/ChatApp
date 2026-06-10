82271e790006799745e3571c1e568c8bd5d2e6d6
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-06-10 12:43:13 +0530

    user appearance sec upated

--- frontend/src/NewPage/NewComponents/SearchSec.tsx
+++ frontend/src/NewPage/NewComponents/SearchSec.tsx
@@ -6,13 +6,13 @@ export default function SearchSec(){
             className="flex justify-center relative"
         >
             <input
-                className="w-full m-3 h-10 border-black rounded-[30px] bg-white text-black p-2"
+                className="w-full m-3 h-[50px] border-black rounded-[30px] bg-slate-700 text-black p-2 pl-10"
                 type="Search" 
-                placeholder="Search User"
+                placeholder="Search Friends To Chat"
             />
             {/* search icon */}
             <div 
-                className="absolute right-3 top-1/2 -translate-y-1/2 pr-4 border-black rounded-[30px] bg-gray-400 p-2 "
+                className="absolute left-3 top-1/2 -translate-y-1/2 pr-4  p-2 text-gray-300"
             >
                 <SearchIcon/>
             </div>
