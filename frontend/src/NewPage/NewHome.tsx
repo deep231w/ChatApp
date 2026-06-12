bdfe7cd0f350c3ecd6c3d2ceafc2cc0bc231d2de
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-06-12 23:49:17 +0530

    sie bar open close responsived for mobile

--- frontend/src/NewPage/NewHome.tsx
+++ frontend/src/NewPage/NewHome.tsx
@@ -13,7 +13,7 @@ export default function NewHome(){
             {/* left sidebar */}
             <div
                 className={`w-1/5 bg-slate-800 flex flex-col relative md:block
-                        ${sideBarOpen ? "block":"hidden"}
+                        ${sideBarOpen ? "block w-full":"hidden"}
                     `}
             >
                 {/* logo */}
@@ -44,7 +44,9 @@ export default function NewHome(){
 
             {/* chat sec */}
             <div
-                className="w-full md:w-4/5  bg-zinc-300 "
+                className={`w-full md:w-4/5  bg-zinc-300
+                        ${sideBarOpen && "hidden"}
+                    `} 
             >
                 <ConversationSec setSideBarOpen={setSideBarOpen}/>   
             </div>
