95bde7d8556a86bc04080e3e2703378a050d1975
Deepak Maharana <100968930+deep231w@users.noreply.github.com>
2026-06-15 01:58:55 +0530

    updated account settings ui

--- frontend/src/NewPage/NewComponents/AccountSettings.tsx
+++ frontend/src/NewPage/NewComponents/AccountSettings.tsx
@@ -2,17 +2,18 @@ import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
+import { CrosshairIcon, CrossIcon, XIcon } from "lucide-react";
 
 export default function AccountSettings(){
     return (
         <Box
             sx={{
-                position: 'absolute',
+                position: 'relative',
                 top: '50%',
                 left: '50%',
                 transform: 'translate(-50%, -50%)',
                 width: 800,
-                height:800,
+                height:600,
                 bgcolor: 'background.paper',
                 boxShadow: 24,
                 p: 4,
@@ -25,7 +26,7 @@ export default function AccountSettings(){
             }}
         >
             <div
-                className="w-1/2 flex flex-col gap-4  items-center border-4 border-indigo-500"
+                className="w-1/2 flex flex-col gap-4 justify-center items-center "
             >
                 <TextField
                     id="standard-basic" label="Standard" variant="standard"
@@ -33,22 +34,36 @@ export default function AccountSettings(){
                 ></TextField>
                 <TextField
                     label="Status"
+                    placeholder="At THe Gym"
                 ></TextField>
                 <TextField
                     label="Email"
                     disabled
                     defaultValue={"xyz@gmail.com"}
                 ></TextField>
-                <Button>SAVE</Button>
+                
+                <Button
+                    
+                    variant="contained"
+                >SAVE</Button>
             </div>
-            <div className="w-1/2 flex justify-center items-center border-4 border-indigo-100">
+            <div className="w-1/2 flex justify-center items-center ">
                 <div
                     className="  rounded-[50%] bg-gray-300 h-[300px] w-[300px] flex justify-center items-center"
                 >
                     Avatar
                 </div>
             </div>
-            
+
+            <div
+                className="absolute top-4 right-4"
+            >
+                <button
+                    
+                >
+                    <XIcon/>
+                </button>
+            </div>            
         </Box>
     )
 }
